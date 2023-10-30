10acaab94ea16941d33dcc1bd7a673c43577e651
Zioth <louisgiet.w@gmail.com>
2023-10-30 22:23:28 +0100

    Fixed wrong place of home.packages

--- hosts/macbook/home-manager/default.nix
+++ hosts/macbook/home-manager/default.nix
@@ -7,48 +7,41 @@
     ../../shared/home-manager/homeConfiguration.nix
   ];
 
-  home.packages = with pkgs; [
-    sequelpro
-  ];
-
-
   home-manager.users."${user}" = {
+    home.packages = with pkgs; [
+      sequelpro
+    ];
+
+    home.sessionVariables = {
+      # EDITOR = "nvim";
+      CLICOLOR = 1;
+      # PAGER = "less"; # idk maybe use bat
+    };
+
     imports = [
       ../../shared/home-manager/user.nix
-      ({ pkgs, ... }: {
-        home.stateVersion = "23.05";
-        # home.packages = with pkgs; [
-        #   # ripgrep
-        # ];
-
-        home.sessionVariables = {
-          # EDITOR = "nvim";
-          CLICOLOR = 1;
-          # PAGER = "less"; # idk maybe use bat
-        };
-
-        # programs.bat = {
-        #   enable = true;
-        #   config.theme = "TwoDark";
-        # };
-        #
-        # programs.fzf = {
-        #   enable = true;
-        #   enableZshIntegration = true;
-        # };
-        #
-        # programs.exa.enable = true;
-        # programs.zsh = {
-        #   enable = true;
-        #   enableCompletion = true;
-        #   enableAutosuggestions = true;
-        #   enableSyntaxHighlighting = true;
-        #   shellAliases = {
-        #     ls = "ls --color=auto -F";
-        #   };
-        # };
-      })
     ];
+
+    # programs.bat = {
+    #   enable = true;
+    #   config.theme = "TwoDark";
+    # };
+    #
+    # programs.fzf = {
+    #   enable = true;
+    #   enableZshIntegration = true;
+    # };
+    #
+    # programs.exa.enable = true;
+    # programs.zsh = {
+    #   enable = true;
+    #   enableCompletion = true;
+    #   enableAutosuggestions = true;
+    #   enableSyntaxHighlighting = true;
+    #   shellAliases = {
+    #     ls = "ls --color=auto -F";
+    #   };
+    # };
 
     home.homeDirectory = pkgs.lib.mkForce "/Users/${user}";
   };
