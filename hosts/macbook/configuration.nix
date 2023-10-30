24ee79b13bb2b66014cb7e86a730f43ec4c06df7
Zioth <louisgiet.w@gmail.com>
2023-10-30 22:07:15 +0100

    Allow unfree on MacOS

--- hosts/macbook/configuration.nix
+++ hosts/macbook/configuration.nix
@@ -9,6 +9,36 @@
 
   services.nix-daemon.enable = true;
 
+  nixpkgs = {
+    # # You can add overlays here
+    # overlays = [
+    #   # Add overlays your own flake exports (from overlays and pkgs dir):
+    #   outputs.overlays.additions
+    #   outputs.overlays.modifications
+    #   # outputs.overlays.unstable-packages
+    #
+    #   # You can also add overlays exported from other flakes:
+    #   # neovim-nightly-overlay.overlays.default
+    #
+    #   # Or define it inline, for example:
+    #   # (final: prev: {
+    #   #   hi = final.hello.overrideAttrs (oldAttrs: {
+    #   #     patches = [ ./change-hello-to-hi.patch ];
+    #   #   });
+    #   # })
+    # ];
+
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      allowInsecure = true;
+      # PermittedInsecurePackages = [
+      #   "python-2.7.18.6"
+      # ];
+    };
+  };
+
   system.stateVersion = 4;
 
   system.defaults = {
