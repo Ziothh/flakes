e7b85990d82a13dd5c80c7654b3d796a7eac929d
Zioth <louisgiet.w@gmail.com>
2024-11-29 14:40:24 +0100

    chore: add a 'path' command to pretty print my $PATH

--- hosts/shared/home-manager/packages/zsh/config/alias.zsh
+++ hosts/shared/home-manager/packages/zsh/config/alias.zsh
@@ -4,17 +4,19 @@
 
 # Basic aliasses
 alias c="clear && printf '\e[3J'"
-alias src="c && source ~/.zshrc && echo 'Reloaded ~/.zshrc'"
+alias src="clear && source ~/.zshrc && echo 'Reloaded ~/.zshrc'"
 
 function mkcd {
   mkdir -p $1
   cd $1
 }
 alias trim="awk '{\$1=\$1;print}'"
-alias ip="ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127. 0.0.1' | sk | trim | pbcopy"
+# alias ip="ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127. 0.0.1' | sk | trim | pbcopy"
 
 alias pbcopy="xclip -selection clipboard"
 alias pbpaste="xclip -selection clipboard -o"
+
+alias path='echo $PATH |  sed "s/:/\\n/g"'
 
 # Open certain config directories in your editor of choice
 # function config {
